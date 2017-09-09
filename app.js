f48520bc745a97ddc124c87c175ae0451bfe9f91
chaosBreaking <x491807573@qq.com>
2017-09-09 16:42:33 +0800

    YouthMap
        基于微信小程序的校园导览小程序，版本号1.1.4。云：Leancloud
        内含代码及图片素材，欢迎改进完善！

---
+++ app.js
@@ -0,0 +1,8 @@
+const AV = require('./utils/av-weapp-min.js');
+// LeanCloud 应用的 ID 和 Key
+AV.init({
+  appId: 'rtWdbzqnmgR4SqSdjjX5LeMH-gzGzoHsz',
+  appKey: 'Cx4tL1sTqAIYvgOcLIo3glaY',
+});
+console.log(AV)
+App({});
